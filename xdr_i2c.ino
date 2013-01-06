c9de1f5fb40ac0a185ce96994fdab43d1e7278db
Konrad Kosmatka <kkonrad92@gmail.com>
2013-01-06 18:57:24 +0100

    GPLv2 commit

--- xdr_i2c.ino
+++ xdr_i2c.ino
@@ -714,14 +714,12 @@ void setup(void)
     delay(5500);
 #endif
 
-  pinMode(SDA_PIN, OUTPUT);
-  pinMode(SCL_PIN, OUTPUT);
-  digitalWrite(SDA_PIN, HIGH);
-  digitalWrite(SCL_PIN, HIGH);
-  
-  delay(250);
-  digitalWrite(RESET_PIN, HIGH);
-  delay(100);
+    digitalWrite(RESET_PIN, HIGH);
+    pinMode(SDA_PIN, OUTPUT);
+    pinMode(SCL_PIN, OUTPUT);
+    digitalWrite(SDA_PIN, HIGH);
+    digitalWrite(SCL_PIN, HIGH);
+    delay(100);
 
 #if DSP_INIT
     dsp_write_data(INIT);
